6d0f28f9c830a59397518cb0d793742286b5702a
承诺一辈子 <48110291+zyx980315@users.noreply.github.com>
2019-04-07 18:15:06 +0800

    Update test_all.py

--- testsuites/test_all.py
+++ testsuites/test_all.py
@@ -1,11 +1,12 @@
 import unittest
 import HTMLTestRunner
 import os
+import sys
 from testsuites.test_luntan import Luntan
 from testsuites.test_luntan2 import Luntan2
 from testsuites.test_luntan3 import Luntan3
 from testsuites.test_luntan4 import Luntan4
-
+sys.append('../')
 cur_path=os.path.dirname(os.path.abspath("."))+"/report"
 report_path=os.path.join(cur_path,"report")
 if not os.path.exists(report_path):os.mkdir(report_path)
